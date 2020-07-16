cad24f1ec18b181c8c82dd9d92f0a3750075c7eb
Johnny Lieu <johnnylieu@yahoo.com>
2020-07-16 12:20:52 -0700

    semicolons after arrays

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Assignment Code
 // var generateBtn = document.querySelector("#generate").addEventListener("click", generatePassword);
-document.querySelector("#generate").addEventListener("click", writePassword);
+document.querySelector("#generate").addEventListener("click", generatePassword);
 
 // Arrays
-var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialChar = ['!', '@', '$', '%', '&', '+', '-', '.', '?', '~'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 // Variables
 var confirmLength = '';
@@ -16,7 +16,7 @@ var confirmUpperCase;
 var confirmLowerCase;
 
 // How many characters would you like in your password?
-function writePassword() {
+function generatePassword() {
   var confirmLength = prompt("How many characters would you like in your password?");
 }
 

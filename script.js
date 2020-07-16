8cdb44d0ba6b0b063dee1b2ff8aa5cec5de5ff2a
Johnny Lieu <johnnylieu@yahoo.com>
2020-07-15 19:42:31 -0700

    trying to get button to generate prompt

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate").addEventListener("click", generatePassword);
+document.querySelector("#generate").addEventListener("click", writePassword);
 
 // Arrays
 var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var specialChar = ['!', '@', '#', '$', '^', '*', '(', ')', '%', '&',', '*', '+', '-', '.', '/', '\', '<', ''>', '?','~'];
+var specialChar = ['!', '@', '$', '%', '&', '+', '-', '.', '?','~'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-// Declaration
-var confirmLength = '';
+// Variables
+var confirmLength;
 var confirmSpecialChars;
 var confirmNumbers;
 var confirmUpperCase;
-Var confirmLowerCase;
+var confirmLowerCase;
 
 // How many characters would you like in your password?
-function genderatePassword () {
-  var confirmLength = prompt("How many characters would you like in your password?")
+function writePassword () {
+  var confirmLength = prompt("How many characters would you like in your password?");
+
+// Loop back if not valid
+if(confirmLength <= 7 || confirmLength >= 129) {
+  alert("Password must be within 8 to 128 characters, please try again.");
+  var confirmLength = prompt("How many characters would you like in your password?");
 }
+
+// Confirm characters
+  alert("Your password length is " + confirmLength + " characters");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// }
